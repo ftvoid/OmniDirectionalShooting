f37fdbf0884c57647662ac3f3e9db32020a662ac
ftvoid <gensaicuttlefish@gmail.com>
2018-02-11 17:18:08 +0900

    コリジョンクラスを汎用化。

---
+++ Assets/Scripts/Object/StageObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageObject : MonoBehaviour {
+    [SerializeField]
+    private float _attack = 100;
+
+    [SerializeField]
+    private float _life = 100;
+
+    public void AddDamage(float attack) {
+        _life -= attack;
+
+        if ( _life <= 0 ) {
+            // TODO : 爆発処理実装
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        var obj = collision.GetComponent<StageObject>();
+        if ( obj == null ) {
+            return;
+        }
+
+        // 衝突対象にダメージを与える
+        obj.AddDamage(_attack);
+    }
+}
