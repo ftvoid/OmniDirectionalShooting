fceb695ec44c7e3d19bbec9bf7b751394fa2c49a
ftvoid <gensaicuttlefish@gmail.com>
2018-02-11 14:54:55 +0900

    回転操作用マーカーを実装。

---
+++ Assets/Scripts/Player/RotationMarkerMover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationMarkerMover : MonoBehaviour {
+    [SerializeField]
+    private Transform _player;
+
+    [SerializeField]
+    private Vector2 _offset = Vector2.zero;
+
+    public void Update() {
+        transform.position = _player.position + (Vector3)_offset;
+    }
+}
