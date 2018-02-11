ccda7c6e5f7d211e51a1a887bf030397bc9dd0ed
ftvoid <gensaicuttlefish@gmail.com>
2018-02-11 14:26:56 +0900

    自機ショットを実装。

---
+++ Assets/Scripts/Player/PlayerShooter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooter : MonoBehaviour {
+    [SerializeField]
+    private PlayerShotMover _shot;
+
+    [SerializeField]
+    private float _reloadTime = 0.5f;
+
+    [SerializeField]
+    private float _shootSpeed = 1f;
+
+    private float _lastShootTime;
+
+    private void Awake() {
+        _lastShootTime = 0;
+    }
+
+    private void Update() {
+        if ( InputManager.IsPressed ) {
+            var time = Time.time;
+            if ( time >= _lastShootTime + _reloadTime ) {
+                _lastShootTime = time;
+
+                var shot = Instantiate(_shot, transform.position, transform.rotation);
+                var angle = transform.eulerAngles.z;
+
+                shot.Velocity = Quaternion.Euler(0, 0, angle) * Vector2.up * _shootSpeed;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerShotMover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShotMover : MonoBehaviour {
+    [SerializeField]
+    private Vector2 _velocity = Vector2.zero;
+
+    public Vector2 Velocity {
+        get { return _velocity; }
+        set {
+            _velocity = value;
+            var angle = Mathf.Atan2(value.y, value.x) * Mathf.Rad2Deg - 90;
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+
+    private void Update() {
+        transform.position += (Vector3)Velocity * Time.deltaTime;
+    }
+}
