e06694055e7aaf99dcc74dc4e5e141160b46ebc0
ftvoid <gensaicuttlefish@gmail.com>
2018-02-11 11:55:48 +0900

    InputManagerクラスのタップ判定処理を実装。

--- Assets/Scripts/Common/InputManager.cs
+++ Assets/Scripts/Common/InputManager.cs
@@ -1,16 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
-public class InputManager : MonoBehaviour {
+/// <summary>
+/// 入力管理クラス
+/// </summary>
+public class InputManager : SingletonMonoBehaviour<InputManager> {
+    private Subject<Unit> _onDown = new Subject<Unit>();
+    private Subject<Unit> _onUp = new Subject<Unit>();
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static bool IsPressed => Input.GetMouseButton(0);
+    public static IObservable<Unit> OnDown => Instance._onDown;
+    public static IObservable<Unit> OnUp => Instance._onUp;
+
+    protected override void Awake() {
+        base.Awake();
+
+        this.ObserveEveryValueChanged(_ => IsPressed)
+            .Subscribe(x => {
+                if ( x ) {
+                    _onDown.OnNext(Unit.Default);
+                } else {
+                    _onUp.OnNext(Unit.Default);
+                }
+            });
+    }
 }
