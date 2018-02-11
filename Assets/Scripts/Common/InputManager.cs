a6562d9514ceb1889834f5c8373adefe95ed544c
ftvoid <gensaicuttlefish@gmail.com>
2018-02-11 12:06:12 +0900

    ドラッグ判定処理を実装。 テスト用スクリプトフォルダを追加。

--- Assets/Scripts/Common/InputManager.cs
+++ Assets/Scripts/Common/InputManager.cs
@@ -10,10 +10,13 @@ using UniRx;
 public class InputManager : SingletonMonoBehaviour<InputManager> {
     private Subject<Unit> _onDown = new Subject<Unit>();
     private Subject<Unit> _onUp = new Subject<Unit>();
+    private Subject<Vector2> _onDrag = new Subject<Vector2>();
 
     public static bool IsPressed => Input.GetMouseButton(0);
+    public static Vector2 Position => Input.mousePosition;
     public static IObservable<Unit> OnDown => Instance._onDown;
     public static IObservable<Unit> OnUp => Instance._onUp;
+    public static IObservable<Vector2> OnDrag => Instance._onDrag;
 
     protected override void Awake() {
         base.Awake();
@@ -26,5 +29,10 @@ public class InputManager : SingletonMonoBehaviour<InputManager> {
                     _onUp.OnNext(Unit.Default);
                 }
             });
+
+        // TODO : Thresholdの実装はこれから
+        this.ObserveEveryValueChanged(_ => Position)
+            .Where(_ => IsPressed)
+            .Subscribe(x => _onDrag.OnNext(x));
     }
 }
---
+++ Assets/Test/InputTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class InputTest : MonoBehaviour {
+    private void Start() {
+        InputManager.OnDown.Subscribe(_ => Debug.Log("OnDown")).AddTo(this);
+        InputManager.OnUp.Subscribe(_ => Debug.Log("OnUp")).AddTo(this);
+        InputManager.OnDrag.Subscribe(x => Debug.Log("OnDrag : " + x.ToString())).AddTo(this);
+    }
+}
