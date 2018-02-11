27d7cae63a7602e2cd256dfa0d9c2911955d7009
ftvoid <gensaicuttlefish@gmail.com>
2018-02-11 12:27:03 +0900

    InputManager内部リファクタリング。 ドラッグイベントのパラメータ拡張。

--- Assets/Scripts/Common/InputManager.cs
+++ Assets/Scripts/Common/InputManager.cs
@@ -8,31 +8,53 @@ using UniRx;
 /// 入力管理クラス
 /// </summary>
 public class InputManager : SingletonMonoBehaviour<InputManager> {
+    public struct DragParam {
+        public Vector2 Position;
+        public Vector2 Delta;
+    }
+
     private Subject<Unit> _onDown = new Subject<Unit>();
     private Subject<Unit> _onUp = new Subject<Unit>();
-    private Subject<Vector2> _onDrag = new Subject<Vector2>();
+    private Subject<DragParam> _onDrag = new Subject<DragParam>();
 
     public static bool IsPressed => Input.GetMouseButton(0);
     public static Vector2 Position => Input.mousePosition;
     public static IObservable<Unit> OnDown => Instance._onDown;
     public static IObservable<Unit> OnUp => Instance._onUp;
-    public static IObservable<Vector2> OnDrag => Instance._onDrag;
+    public static IObservable<DragParam> OnDrag => Instance._onDrag;
+
+    private Vector2 _prevPosition;
+    private Vector2 _dragDelta;
 
     protected override void Awake() {
         base.Awake();
 
         this.ObserveEveryValueChanged(_ => IsPressed)
-            .Subscribe(x => {
-                if ( x ) {
-                    _onDown.OnNext(Unit.Default);
-                } else {
-                    _onUp.OnNext(Unit.Default);
-                }
-            });
+            .Subscribe(OnPressChanged);
 
         // TODO : Thresholdの実装はこれから
         this.ObserveEveryValueChanged(_ => Position)
             .Where(_ => IsPressed)
-            .Subscribe(x => _onDrag.OnNext(x));
+            .Subscribe(OnPositionChanged);
+
+        _prevPosition = Position;
+    }
+
+    private void OnPressChanged(bool isPressed) {
+        if ( isPressed ) {
+            _onDown.OnNext(Unit.Default);
+        } else {
+            _onUp.OnNext(Unit.Default);
+        }
+    }
+
+    private void OnPositionChanged(Vector2 pos) {
+        _dragDelta = pos - _prevPosition;
+        _prevPosition = pos;
+
+        _onDrag.OnNext(new DragParam {
+            Position = pos,
+            Delta = _dragDelta,
+        });
     }
 }
