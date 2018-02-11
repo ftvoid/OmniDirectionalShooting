03704b03bc1d0f4be2df58bc29087714593f00cc
ftvoid <gensaicuttlefish@gmail.com>
2018-02-12 01:00:49 +0900

    クラス名を修正。

--- Assets/Scripts/Common/EraseOnHide.cs
+++ Assets/Scripts/Common/EraseOnHide.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HideOnErase : MonoBehaviour {
+public class EraseOnHide : MonoBehaviour {
     private void OnBecameInvisible() {
         Destroy(gameObject);
     }
