ab0f4ebb362bc8bcde1600880afadfeff242c722
ftvoid <gensaicuttlefish@gmail.com>
2018-02-11 14:36:01 +0900

    自機ショットが画面外に来たとき、消す処理を実装。

---
+++ Assets/Scripts/Common/HideOnErase.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideOnErase : MonoBehaviour {
+    private void OnBecameInvisible() {
+        Destroy(gameObject);
+    }
+}
