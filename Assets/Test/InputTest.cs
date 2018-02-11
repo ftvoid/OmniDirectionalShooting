6ba1e8f58aed8b1e42008d66daef915d271a4216
ftvoid <gensaicuttlefish@gmail.com>
2018-02-11 13:25:53 +0900

    テストクラス修正。

--- Assets/Test/InputTest.cs
+++ Assets/Test/InputTest.cs
@@ -7,6 +7,6 @@ public class InputTest : MonoBehaviour {
     private void Start() {
         InputManager.OnDown.Subscribe(_ => Debug.Log("OnDown")).AddTo(this);
         InputManager.OnUp.Subscribe(_ => Debug.Log("OnUp")).AddTo(this);
-        InputManager.OnDrag.Subscribe(x => Debug.Log("OnDrag : " + x.Delta.ToString())).AddTo(this);
+        InputManager.OnDrag.Subscribe(x => Debug.Log("OnDrag : " + x.ToString())).AddTo(this);
     }
 }
